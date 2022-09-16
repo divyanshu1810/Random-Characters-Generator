1a3602bae62ad12b6d7e20ebf475e92af3c0c153
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-16 21:51:00 +0530

    :lipstick: Updated the CSS
    Added a genrate button to the landing page

--- pages/Favourites.tsx
+++ pages/Favourites.tsx
@@ -9,7 +9,7 @@ function favourite() {
           <div className=' space-y-4 myfont text-center'>
           <div>Name : Divyanshu</div>
           <div>Disney Id : 18</div>
-          <div><button className='border-2 bg-blue-900 hover:bg-blue-500 duration-200 rounded-lg p-2 text-xs'>Add to favourites</button></div>
+          <div><button className='border-2 bg-blue-900 hover:bg-blue-500 duration-200 rounded-lg p-2 text-xs'>remove from favourites</button></div>
           </div>
         </div>
         <div className='flex justify-between space-x-56'>
@@ -17,7 +17,7 @@ function favourite() {
           <div className=' space-y-4 myfont text-center'>
           <div>Name : Divyanshu</div>
           <div>Disney Id : 18</div>
-          <div><button className='border-2 bg-blue-900 hover:bg-blue-500 duration-200 rounded-lg p-2 text-xs'>Add to favourites</button></div>
+          <div><button className='border-2 bg-blue-900 hover:bg-blue-500 duration-200 rounded-lg p-2 text-xs'>remove from favourites</button></div>
           </div>
         </div>
         <div className='flex justify-between space-x-56'>
@@ -25,7 +25,7 @@ function favourite() {
           <div className=' space-y-4 myfont text-center'>
           <div>Name : Divyanshu</div>
           <div>Disney Id : 18</div>
-          <div><button className='border-2 bg-blue-900 hover:bg-blue-500 duration-200 rounded-lg p-2 text-xs'>Add to favourites</button></div>
+          <div><button className='border-2 bg-blue-900 hover:bg-blue-500 duration-200 rounded-lg p-2 text-xs'>remove from favourites</button></div>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next'
+import Link from 'next/link';
 const Home: NextPage = ({imageUrl,url,name}:any) => {
   const i = url.lastIndexOf('/')
   const id=url.substring(i+1);
   return (
-      <div className='flex space-y-8 md:space-y-0 md:flex-row h-screen flex-col md:justify-evenly justify-center flex-wrap items-center -mt-20'>
-        <img className=' rounded-xl' src={imageUrl} alt="" width="200" />
+      <div className='flex space-y-8 lg:space-y-0 md:space-y-10 lg:flex-row h-screen flex-col lg:justify-evenly justify-center flex-wrap items-center -mt-10 mb-10 lg:-mt-20' >
+        {imageUrl && <img className=' rounded-xl' src={imageUrl} alt="" width="200" />}
+        <div className='border-2 rounded-lg p-2 bg-blue-900 hover:bg-blue-500 duration-200 cursor-pointer'><Link href="/">Generate</Link> </div>
         <div className=' space-y-4 myfont text-center'>
-          <div>Name : {name}</div>
-          <div>Disney Id : {id}</div>
+          {name && <div className='text-2xl'>Name : {name}</div>}
+          {id && <div className='text-2xl'>Disney Id : {id}</div>}
           <div><button className='border-2 bg-blue-900 hover:bg-blue-500 duration-200 rounded-lg p-2 text-xs'>Add to favourites</button></div>
         </div>
       </div>
